c67d3bc031eb7f8d0c0fdb0eb8c101e4da0795f0
Mike Foxworthy <mike.foxworthy@viasat.com>
2022-03-23 08:53:15 -0700

    Complete rewrite. No more nested while loops

--- flow_broker.py
+++ flow_broker.py
@@ -4,8 +4,7 @@ import sys
 import os
 
 
-def Main():
-    fl_table = {}
+def main():
     s_address = '/var/run/ulogd.sock'
     sc_address = '/var/run/l7stats.sock'
 
@@ -34,72 +33,29 @@ def Main():
        the server to make a connection"""
     s.listen(1)
     sc.listen(1)
-
+    # Waits for a connection
+    print("Waiting for a connection on ulogd")
+    u_conn, ulog_addr = s.accept()
+    print("Waiting for a connection on stats")
+    stats_conn, stats_addr = sc.accept()
+    fh = u_conn.makefile()
     while True:
-        # Waits for a connection
-        print("Waiting for a connection on ulogd")
-        u_conn, ulog_addr = s.accept()
-        fh = u_conn.makefile()
-
-        try:
-            print('connection from', ulog_addr)
-            data = fh.readline()
-            jd = json.loads(data)
-            if jd["ip.protocol"] == 1:
-                continue
-            elif "src_port" not in jd:
-                continue
-            s_data = {"src_ip": jd["src_ip"], "src_port": jd["src_port"], "dest_ip": jd["dest_ip"], "dest_port": jd["dest_port"],
-                      "oob.in": jd["oob.in"], "oob.out": jd["oob.out"], "bytes": jd["ip.totlen"] }
-            s_data = json.dumps(s_data, indent=4)
-            print(s_data)
-            # Receive the data in small chunks and retransmit it
-            while True:
-                print("Waiting for a connection on stats")
-                stats_conn, stats_addr = sc.accept()
-
-                try:
-                    print('connection from', stats_addr)
-                    while True:
-                        stats_conn.sendall(str(s_data).encode("utf-8"))
-                finally:
-                    stats_conn.close()
-                """
-                if jd["oob.in"] != None:
-                    r_ip = jd["src_ip"]
-                    r_port = jd["src_port"]
-                    l_ip = jd["dest_ip"]
-                    l_port = jd["dest_port"]
-                    rx = jd["ip.totlen"]
-                    fl_hash = (r_ip, r_port, l_ip, l_port)
-                    fl_hash = str(hash(fl_hash))
-                    if fl_hash in fl_table and "rx" in fl_table[fl_hash].keys():
-                        fl_table[fl_hash]["rx"] += rx
-                    elif fl_hash not in fl_table:
-                        fl_table.update({fl_hash: {"rx": 0}})
-                    else:
-                        fl_table[fl_hash]["rx"] = rx
-                if jd["oob.out"] != None:
-                    r_ip = jd["dest_ip"]
-                    r_port = jd["dest_port"]
-                    l_ip = jd["src_ip"]
-                    l_port = jd["src_port"]
-                    tx = jd["ip.totlen"]
-                    fl_hash = (l_ip, l_port, r_ip, r_port)
-                    fl_hash = str(hash(fl_hash))
-                    if fl_hash in fl_table and "tx" in fl_table[fl_hash].keys():
-                        fl_table[fl_hash]["tx"] += tx
-                    elif fl_hash not in fl_table:
-                        fl_table.update({fl_hash: {"tx": 0}})
-                    else:
-                        fl_table[fl_hash]["tx"] = tx
-                print(fl_table)
-                """
-        finally:
-                # Free the connection
-            u_conn.close()
-
+        data = fh.readline()
+        jd = json.loads(data)
+        if jd["ip.protocol"] == 1:
+            continue
+        elif "src_port" not in jd:
+            continue
+        s_data = {"src_ip": jd["src_ip"], "src_port": jd["src_port"], "dest_ip": jd["dest_ip"],
+                  "dest_port": jd["dest_port"],
+                  "oob.in": jd["oob.in"], "oob.out": jd["oob.out"], "bytes": jd["ip.totlen"]}
+        s_data = json.dumps(s_data)
+        s_data = s_data + "\n"
+        # print(s_data)
+        stats_conn.sendall(str(s_data).encode("utf-8"))
+    u_conn.close()
+    stats_conn.close()
 
 
 if __name__ == "__main__":
-    Main()
+    main()
