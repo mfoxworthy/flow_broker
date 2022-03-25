00608c481aad9be099e624c6680b4ed6d2b4a2c7
Mike Foxworthy <mike.foxworthy@viasat.com>
2022-03-25 08:53:16 -0700

    Completed syslog TODO. Syslog is now working.

--- flow_broker.py
+++ flow_broker.py
@@ -4,27 +4,30 @@ import errno
 import os
 from queue import Queue
 from threading import Thread
+from syslog import \
+    openlog, syslog, LOG_PID, LOG_PERROR, LOG_DAEMON, \
+    LOG_DEBUG, LOG_ERR, LOG_WARNING, LOG_INFO
 
 
-# TODO create logger functions
 def server(sq):
     path = "/var/run/l7stats.sock"
     try:
         os.unlink(path)
-    except OSError:
+    except OSError as e:
+        syslog(LOG_ERR, f"Cannot unlink file: {path}")
         if os.path.exists(path):
             raise
     s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
-    print("Starting up on {}".format(path))
+    syslog(LOG_INFO, "Starting up stats socket")
     s.bind(path)
     s.listen()
     disconn = True
     while True:
         if disconn:
-            print("Waiting for a connection on stats")
+            syslog(LOG_INFO, f"Waiting for a connection on {s}")
             conn, addr = s.accept()
-            print("Accepted connection on stats")
+            syslog(LOG_INFO, f"Accepted connection on {s}")
             disconn = False
         while True:
             msg = sq.get()
@@ -33,7 +36,7 @@ def server(sq):
             try:
                 conn.sendall(msg)
             except Exception as e:
-                print("Could not send data")
+                syslog(LOG_ERR, f"Could not send data on {conn}")
                 disconn = True
                 break
         conn.close()
@@ -43,20 +46,20 @@ def pkt_thread(sq):
     disconn = True
     p = socket.socket()
 
-    print("Starting up on {}".format(p))
+    syslog(LOG_INFO, "Starting up on pkt socket")
     p.bind(('127.0.0.1', 6001))
 
     p.listen()
 
     while True:
         if disconn:
-            print("Waiting for a connection on ulogd_p")
+            syslog(LOG_INFO, f"Waiting for a connection on {p}")
             p_conn, ulog_addr = p.accept()
-            print("Accepted connection from: ulogd_p")
+            syslog(LOG_INFO, f"Accepted connection from: {p}")
             try:
                 p_fh = p_conn.makefile()
             except Exception as e:
-                print("Cannot read socket: Restarting")
+                syslog(LOG_ERR, f"Cannot read socket: Restarting {p_conn}")
             disconn = True
         while True:
             try:
@@ -79,7 +82,7 @@ def pkt_thread(sq):
                           "oob.in": p_jd["oob.in"], "oob.out": p_jd["oob.out"], "bytes": p_jd["ip.totlen"]}
             except Exception as e:
                 p_data = {"KeyError": e}
-                print("Must have a KeyError with: ", e)
+                syslog(LOG_ERR, f"Must have a KeyError with: {e}")
                 continue
             p_data = json.dumps(p_data)
             p_data = p_data + "\n"
@@ -95,20 +98,20 @@ def flow_thread(sq):
     disconn = True
     f = socket.socket()
 
-    print("Starting up on {}".format(f))
+    syslog(LOG_INFO, "Starting up flow socket")
     f.bind(('127.0.0.1', 6000))
 
     f.listen()
 
     while True:
         if disconn:
-            print("Waiting for a connection on ulogd_f")
+            syslog(LOG_INFO, f"Waiting for a connection on {f}")
             f_conn, ulog_addr = f.accept()
-            print("Accepted connection from: ulogd_f")
+            syslog(LOG_INFO, f"Accepted connection from: {f}")
             try:
                 f_fh = f_conn.makefile()
             except Exception as e:
-                print("Cannot read socket: Restarting")
+                syslog(LOG_ERR, f"Cannot read socket: Restarting {f_conn}")
             disconn = True
         while True:
             try:
@@ -122,10 +125,10 @@ def flow_thread(sq):
 
             f_jd = json.loads(f_data)
             if f_jd is None:
-                print("We have no data")
+                syslog(LOG_DEBUG, "We have no data on f_jd")
                 continue
             if "orig.ip.protocol" not in f_jd.keys():
-                print("Still no data")
+                syslog(LOG_DEBUG, "Still no data in f_jd")
                 continue
             if f_jd["orig.ip.protocol"] == 1:
                 continue
@@ -136,7 +139,7 @@ def flow_thread(sq):
                               "r_port": f_jd["reply.l4.sport"], "purge": 1}
                 except Exception as e:
                     f_data = {"KeyError": e}
-                    print("Must have a KeyError with: ", e)
+                    syslog(LOG_ERR, f"Must have a KeyError with: {e}")
                     continue
                 f_data = json.dumps(f_data)
                 f_data = f_data + "\n"
@@ -153,7 +156,7 @@ if __name__ == "__main__":
     s_proc = Thread(target=server, args=(q,), daemon=True)
     p_proc = Thread(target=pkt_thread, args=(q,))
     f_proc = Thread(target=flow_thread, args=(q,))
-
+    syslog(LOG_INFO, "Flow Broker Starting")
     s_proc.start()
     p_proc.start()
     f_proc.start()
